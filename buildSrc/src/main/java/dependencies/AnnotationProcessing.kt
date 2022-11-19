aaf089fbdb2047cc48390d7c6e25758a54acbbdf
Tadese10 <ytajudeen23@yahoo.com>
2022-11-19 16:09:03 +0100

    Added dependecies to the project

---
+++ buildSrc/src/main/java/dependencies/AnnotationProcessing.kt
@@ -0,0 +1,7 @@
+package dependencies
+
+object AnnotationProcessing {
+    val lifecycle_compiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle_version}"
+    val room_compiler = "androidx.room:room-compiler:${Versions.room}"
+    val dagger_compiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+}
---
+++ buildSrc/src/main/java/dependencies/TestDependencies.kt
@@ -0,0 +1,10 @@
+package dependencies
+
+object TestDependencies {
+    val mockk = "io.mockk:mockk:${Versions.mockk_version}"
+    val jupiter_api = "org.junit.jupiter:junit-jupiter-api:${Versions.junit_jupiter_version}"
+    val jupiter_params = "org.junit.jupiter:junit-jupiter-params:${Versions.junit_jupiter_version}"
+    val jupiter_engine = "org.junit.jupiter:junit-jupiter-engine:${Versions.junit_jupiter_version}"
+
+    val junit4 = "junit:junit:${Versions.junit_4_version}"
+}
